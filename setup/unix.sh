915fee9bba9037f413ccaa5c780b15a4f6022c4f
MatthewFro191919 <matthewfroncek4@gmail.com>
2025-05-10 05:04:31 -0400

    Update unix.sh

--- setup/unix.sh
+++ setup/unix.sh
@@ -10,6 +10,7 @@ echo This might take a few moments depending on your internet speed.
 haxelib install flixel 5.6.1
 haxelib install flixel-addons 3.2.2
 haxelib install flixel-tools 1.5.1
+haxelib install flixel-ui
 haxelib install hscript-iris 1.1.3
 haxelib install tjson 1.4.0
 haxelib install hxdiscord_rpc 1.2.4
